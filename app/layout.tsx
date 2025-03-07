4e0e8e147729ec143219231c18b1beefdffc60c8
krupalll <krupalkumarpatel.21.cs@iite.indusuni.ac.in>
2025-03-07 12:29:20 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "Talksphere",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
@@ -25,10 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <ClerkProvider
         appearance={{
-          layout: {
-            socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
-          },
           variables: {
             colorText: "#fff",
             colorPrimary: "#0E78F9",
